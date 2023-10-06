641efdaba0ac27e9a8a5e43bc6b616d92aaadb8e
karlthomas3 <114147680+karlthomas3@users.noreply.github.com>
2023-10-06 15:50:25 +0100

    turned on test data uploader for demo

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -3,7 +3,7 @@
 import styled from 'styled-components';
 import Logo from './Logo';
 import MainNav from './MainNav';
-// import Uploader from '../data/Uploader';
+import Uploader from '../data/Uploader';
 
 const StyledSidebar = styled.aside`
 	background-color: var(--color-grey-0);
@@ -22,7 +22,7 @@ function Sidebar() {
 			<Logo />
 			<MainNav />
 
-			{/* <Uploader /> */}
+			<Uploader />
 		</StyledSidebar>
 	);
 }
